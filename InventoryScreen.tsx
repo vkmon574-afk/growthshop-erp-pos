6656f04bd0533b7a675065025d7def5acf9aa973
vkmon574-afk <vkmon574@gmail.com>
2026-08-10 09:22:22 +0530

    Update InventoryScreen.tsx

--- InventoryScreen.tsx
+++ InventoryScreen.tsx
@@ -129,11 +129,10 @@ const filteredItems = products.filter((item: any) => {
     setCategory(item.category as SaleCategory);
     setPrice(item.selling_price.toString());
     setUnit(item.unit);
-    setStockQty(item.stock_qty.toString())
+    setStockQty(item.stock_qty.toString());
     setBarcode(item.barcode || '');
     setShowAddModal(true);
-  }
-
+  };
   return (
     <div className="flex flex-col w-full max-w-lg mx-auto px-container-margin pt-stack-md pb-28 animate-fadeIn gap-section-gap">
       {/* Header Banner */}
